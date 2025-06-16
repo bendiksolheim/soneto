854612a7dcbde7e897794c9a9b8432fa3755bee3
Bendik Solheim <hello@bendik.dev>
2025-06-16 22:58:14 +0200

    Add PostHog

--- next.config.js
+++ next.config.js
@@ -10,7 +10,26 @@ const nextConfig = {
       "c.tiles.mapbox.com",
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://eu-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://eu.i.posthog.com/:path*",
+      },
+      {
+        source: "/ingest/decide",
+        destination: "https://eu.i.posthog.com/decide",
+      },
+    ];
+  },
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
 };
+
 // webpack: (config) => {
 //   // Support for mapbox-gl which uses worker-loader
 //   config.module.rules.push({

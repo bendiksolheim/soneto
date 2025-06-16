967d465b2d2b8c2440439275fe06053746f0f18f
Bendik Solheim <hello@bendik.dev>
2025-06-16 23:07:33 +0200

    Don’t store PostHog info in cookie

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -3,6 +3,7 @@ import posthog from "posthog-js";
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
   api_host: "/ingest",
   ui_host: process.env.NEXT_PUBLIC_POSTHOG_HOST!,
+  persistence: "localStorage",
   capture_pageview: "history_change",
   capture_pageleave: true, // Enable pageleave capture
   capture_exceptions: true, // This enables capturing exceptions using Error Tracking, set to false if you don't want this
